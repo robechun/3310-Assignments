155f7b275d0a1e50dddd43c0b5be622d8dc51735
Robert Chung <robertchung97@gmail.com>
2017-11-20 19:03:10 -0800

    Done with Scheme assignment

--- HW_Scheme/assignment.scm
+++ HW_Scheme/assignment.scm
@@ -1,17 +1,19 @@
-;;; Questions:
-;;;    What about like base 12? aka invalid bases
-;;;    Is this method legal for this class
-;;;    Return? or just display? for all of these things
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;      Robert Chung                        ;;;
+;;;      Dr. Arias                           ;;;
+;;;      Concepts in Programming Languages   ;;;
+;;;      Due 11/29/17                        ;;;
+;;;      Scheme assignment                   ;;;
+;;;      assignment.scm                      ;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+
+;;; Convert number (base 10) to a specified base
 (define (convert number base)
-    (cond 
-        ((= base 2)  (number->string number base)) 
-        ((= base 8)  (number->string number base))
-        ((= base 16) (number->string number base))
-        (else (display "False"))
-    )
+    (display (number->string number base))
 )
 
+;;; Roll-till-you-win Rolls two die until they match
 (define (roll-till-you-win)
     (let (
             (die-roll (1+ (random 6)))
@@ -28,10 +30,11 @@
 ;;; list-primes prints out all the primes up to n
 (define (list-primes n)
     (let (
-            (primeVec (make-vector n #t)) ;Vector to keep track of primes
+            (primeVec (make-vector n #t)) ; Vector to keep track of primes
         )
         (do ((i 2 (1+ i))) ; Loop and find multiples of primes and set them to be false
             ((>= (* i i) n))
+
             (if (equal? #t (vector-ref primeVec i))
                 (do ((j (* i 2) (+ j i)))
                 ((>= j n))
